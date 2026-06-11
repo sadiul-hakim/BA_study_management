c7cbb39ca41e7f5d5865eb9ff5256b394df4f350
Sadi ul Hakim <sadiulhakimpersonal@gmail.com>
2026-06-11 21:24:07 +0600

    Theme

--- BA_study_management/settings.py
+++ BA_study_management/settings.py
@@ -46,6 +46,10 @@ INSTALLED_APPS = [
     'document'
 ]
 
+JAZZMIN_SETTINGS = {
+    "show_theme_chooser": True,
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
