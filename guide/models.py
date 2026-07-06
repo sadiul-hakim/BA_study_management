10b226dd18f12e6d6b93f53f9fb42d118d3f4aa5
Sadiul Hakim <sadiulhakim@gmail.com>
2026-07-06 16:46:46 +0600

    db

--- guide/models.py
+++ guide/models.py
@@ -2,11 +2,29 @@ from django.db import models
 
 
 class Guide(models.Model):
+    PRIMARY = 1
+    WARNING = 2
+    DANGER = 3
+
+    TYPE = [
+        (PRIMARY, "Primary"),
+        (WARNING, "Warning"),
+        (DANGER, "Danger"),
+    ]
+
     text = models.TextField()
     priority = models.IntegerField(default=0)
+    type = models.IntegerField(choices=TYPE, default=WARNING)
 
     class Meta:
         ordering = ['priority']
 
     def __str__(self):
         return f"Guide {self.priority}"
+
+    def bootstrap_class(self):
+        return {
+            self.PRIMARY: "card-primary",
+            self.WARNING: "card-warning",
+            self.DANGER: "card-danger",
+        }.get(self.type, "card-secondary")
